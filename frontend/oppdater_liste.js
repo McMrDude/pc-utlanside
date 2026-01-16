a17ead09e9711ac773f3ba84d319ef528ced87aa
Mr penington <benjaminbkris@gmail.com>
2026-01-16 10:00:50 +0100

    Update oppdater_liste.js

--- frontend/oppdater_liste.js
+++ frontend/oppdater_liste.js
@@ -25,14 +25,14 @@ async function openList() {
   list.style.display = "block";
   calendar.style.display = "none";
   popup.style.display = "none";
-  ument.getElementById("pcPage").style.display = "none";
+  document.getElementById("pcPage").style.display = "none";
 }
 
 async function openCalendar() {
   list.style.display = "none";
   calendar.style.display = "block";
   popup.style.display = "none";
-  ument.getElementById("pcPage").style.display = "none";
+  document.getElementById("pcPage").style.display = "none";
 
   if (!calendarInstance) {
     calendarInstance = new FullCalendar.Calendar(calendar, {
