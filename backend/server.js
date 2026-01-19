a1534886aaff19c3db28a7919ff7c1f1424d14e1
Mr penington <benjaminbkris@gmail.com>
2026-01-19 14:42:29 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -249,7 +249,7 @@ app.get("/pcs/status", async (req, res) => {
 /* =========================
    Start server
 ========================= */
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
