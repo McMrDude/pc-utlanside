5c14f124b0471780e0646dd7400dfd557c49c2cc
Mr penington <benjaminbkris@gmail.com>
2026-05-08 10:10:29 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -361,7 +361,7 @@ app.get("/availability", async (req, res) => {
     const loaned = await pool.query(`
       SELECT COUNT(DISTINCT pc_number)
       FROM rentals
-      status = 'active'
+      WHERE status = 'active'
     `);
 
     const available =
