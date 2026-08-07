2eea4e3f9e182c15b9ffb4d31e3fdfc3f0aad533
Mr penington <benjaminbkris@gmail.com>
2026-08-07 13:41:28 +0200

    Update oppdater_liste.js

--- frontend/oppdater_liste.js
+++ frontend/oppdater_liste.js
@@ -448,6 +448,7 @@ async function loadCalendarEvents() {
         title: `${r.student_name} - PC ${r.pc_number}`,
         start: returnDate.toISOString().split("T")[0],
         display: "background",
+        style: "padding: 10px;",
         color: color,
         classNames: daysRemaining === 0 ? ["today-rental"] : [],
         extendedProps: {
@@ -459,7 +460,7 @@ async function loadCalendarEvents() {
           pcNumber: r.pc_number
         }
       });
-      calendarInstance.style = "padding: 10px;";
+      calendarInstance.style = ""
     };
   });
   const todayButton = document.getElementsByClassName("fc-today-button")[0];
